61997b2465ad960efd2f106a5a23b7363e7a12ce
Mathieu <mdeharbe@hotmail.com>
2020-07-29 21:47:52 +0200

    base de la sélection d'un choix

--- venv/exec/execEffet.py
+++ venv/exec/execEffet.py
@@ -8,8 +8,24 @@ class ExecEffet(ExecNoeud):
         pass
 
     def LancerNoeud(self):
+        ExecNoeud.LancerNoeud(self)
+
+        #affichage du texte
         print(self.m_Effet)
-        index = 1
-        for choix in self.m_Effet.ParcourirChoix():
-            print("{} - {}".format(index, choix))
-            index+=1
+
+        if (len(self.m_Effet.m_Choix) > 1):  # il y a un choix qui se pose :
+            index = 1
+            for choix in self.m_Effet.ParcourirChoix():
+                print("{} - {}".format(index, choix))
+                index+=1
+            indexChoisi = index
+            choisi = False
+            while(not choisi):
+                indexChoisi = input("Que souhaitez vous faire ?")
+                try:
+                    indexChoisi = int(indexChoisi)
+                except ValueError:
+                    indexChoisi = -1
+                choisi = indexChoisi > 0 and indexChoisi < index
+                if not choisi:
+                    print("Choisissez un numéro parmi les choix proposés.")
--- venv/exec/execNoeud.py
+++ venv/exec/execNoeud.py
@@ -2,5 +2,14 @@
 
 class ExecNoeud:
 
+    def ExecuterActionsNoeud(self, noeudAExecuter):
+        """
+        exécute tout ce qui est défini dans ce noeud (changements de caracs, go to un autre effet etc...
+        :param noeudAExecuter:
+        """
+
+        pass
+
     def LancerNoeud(self):
-        print("devrait être surclassé je pense !!!");
+
+        pass
